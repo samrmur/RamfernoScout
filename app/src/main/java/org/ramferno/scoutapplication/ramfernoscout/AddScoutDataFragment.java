ec0c31097774a5dcdaa9af112e47738841db0955
samrmur <sameralabi@hotmail.com>
2016-04-15 02:07:51 -0400

    tenth  commit.

--- app/src/main/java/org/ramferno/scoutapplication/ramfernoscout/AddScoutDataFragment.java
+++ app/src/main/java/org/ramferno/scoutapplication/ramfernoscout/AddScoutDataFragment.java
@@ -1,11 +1,9 @@
 package org.ramferno.scoutapplication.ramfernoscout;
 
-import android.content.DialogInterface;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
