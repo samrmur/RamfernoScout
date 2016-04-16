2a295a643fab80975aea1e6128af9c391cdeaa3e
samrmur <sameralabi@hotmail.com>
2016-04-16 00:25:26 -0400

    Replaced EditText objects with CheckBox objects for ScoutFragment.

--- app/src/main/java/org/ramferno/scoutapplication/ramfernoscout/AddScoutDataFragment.java
+++ app/src/main/java/org/ramferno/scoutapplication/ramfernoscout/AddScoutDataFragment.java
@@ -8,6 +8,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -19,9 +21,12 @@ public class AddScoutDataFragment extends Fragment {
     Button cancelButton;
     Button addDataButton;
     DatabaseHelper myDB;
-    EditText tNumber, tPortcullis, tChevalFrise, tMoat, tRamparts, tDrawbridge, tSallyPort, tRockWall, tRockTerrain, tLowBar, tAutoHigh, tAutoLow,
-            tTeleHigh, tTeleLow, tTelePlay, tHang;
+    EditText tNumber, tTelePlay;
+    CheckBox tPortcullis, tChevalFrise, tMoat, tRamparts, tDrawbridge, tSallyPort, tRockWall, tRockTerrain, tLowBar, tAutoHigh, tAutoLow,
+            tTeleHigh, tTeleLow, tHang;
     SQLiteDatabase sqLiteDatabase;
+    String fPorticullis, fChevalFrise, fMoat, fRamparts, fDrawbridge, fSallyPort, fRockWall, fRockTerrain, fLowBar, fAutoHigh, fAutoLow,
+            fTeleHigh, fTeleLow, fHang;
 
     public AddScoutDataFragment() {
         // Required empty public constructor
@@ -34,23 +39,172 @@ public class AddScoutDataFragment extends Fragment {
         //Starts database
         myDB = new DatabaseHelper(getActivity());
 
+        //Sets default value of Strings
+        fPorticullis = "No";
+        fChevalFrise = "No";
+        fMoat = "No";
+        fRamparts = "No";
+        fDrawbridge = "No";
+        fSallyPort = "No";
+        fRockWall = "No";
+        fRockTerrain = "No";
+        fLowBar= "No";
+        fAutoHigh = "No";
+        fAutoLow = "No";
+        fTeleHigh = "No";
+        fTeleLow = "No";
+        fHang = "No";
+
         //Instantiate all editText objects
         tNumber = (EditText) view.findViewById(R.id.editNumber);
-        tPortcullis = (EditText) view.findViewById(R.id.editPortcullis);
-        tChevalFrise = (EditText) view.findViewById(R.id.editChevalFrise);
-        tMoat = (EditText) view.findViewById(R.id.editMoat);
-        tRamparts = (EditText) view.findViewById(R.id.editRamparts);
-        tDrawbridge = (EditText) view.findViewById(R.id.editDrawbridge);
-        tSallyPort = (EditText) view.findViewById(R.id.editSallyPort);
-        tRockWall = (EditText) view.findViewById(R.id.editRockWall);
-        tRockTerrain = (EditText) view.findViewById(R.id.editRockTerrain);
-        tLowBar = (EditText) view.findViewById(R.id.editLowBar);
-        tAutoHigh = (EditText) view.findViewById(R.id.editHighGoal);
-        tAutoLow = (EditText) view.findViewById(R.id.editLowGoal);
-        tTeleHigh = (EditText) view.findViewById(R.id.editHighGoalOp);
-        tTeleLow = (EditText) view.findViewById(R.id.editLowGoalOp);
+        tPortcullis = (CheckBox) view.findViewById(R.id.editPortcullis);
+        tChevalFrise = (CheckBox) view.findViewById(R.id.editChevalFrise);
+        tMoat = (CheckBox) view.findViewById(R.id.editMoat);
+        tRamparts = (CheckBox) view.findViewById(R.id.editRamparts);
+        tDrawbridge = (CheckBox) view.findViewById(R.id.editDrawbridge);
+        tSallyPort = (CheckBox) view.findViewById(R.id.editSallyPort);
+        tRockWall = (CheckBox) view.findViewById(R.id.editRockWall);
+        tRockTerrain = (CheckBox) view.findViewById(R.id.editRockTerrain);
+        tLowBar = (CheckBox) view.findViewById(R.id.editLowBar);
+        tAutoHigh = (CheckBox) view.findViewById(R.id.editHighGoal);
+        tAutoLow = (CheckBox) view.findViewById(R.id.editLowGoal);
+        tTeleHigh = (CheckBox) view.findViewById(R.id.editHighGoalOp);
+        tTeleLow = (CheckBox) view.findViewById(R.id.editLowGoalOp);
         tTelePlay = (EditText) view.findViewById(R.id.editOffenseDefense);
-        tHang = (EditText) view.findViewById(R.id.editHang);
+        tHang = (CheckBox) view.findViewById(R.id.editHang);
+
+        //Checks checkbox for values then converts values into strings
+        tPortcullis.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)  {
+                if(tPortcullis.isChecked()) {
+                    fPorticullis = "Yes";
+                }
+                else {
+                    fPorticullis = "No";
+                } //End of if statement
+            } //End of onClick
+        }); //End of onClickListener
+
+        //Checks checkbox for values then converts values into strings
+        tMoat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(tMoat.isChecked()) {
+                    fMoat = "Yes";
+                } //End of if statement
+            } //End of onClick
+        }); //End of onClickListener
+
+        //Checks checkbox for values then converts values into strings
+        tRamparts.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(tRamparts.isChecked()) {
+                    fRamparts = "Yes";
+                } //End of if statement
+            } //End of onClick
+        }); //End of onClickListener
+
+        //Checks checkbox for values then converts values into strings
+        tDrawbridge.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(tDrawbridge.isChecked()) {
+                    fDrawbridge = "Yes";
+                } //End of if statement
+            } //End of onClick
+        }); //End of onClickListener
+
+        //Checks checkbox for values then converts values into strings
+        tSallyPort.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(tSallyPort.isChecked()) {
+                    fSallyPort = "Yes";
+                } //End of if statement
+            } //End of onClick
+        }); //End of onClickListener
+
+        //Checks checkbox for values then converts values into strings
+        tRockWall.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(tRockWall.isChecked()) {
+                    fRockWall = "Yes";
+                } //End of if statement
+            } //End of onClick
+        }); //End of onClickListener
+
+        //Checks checkbox for values then converts values into strings
+        tRockTerrain.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(tRockTerrain.isChecked()) {
+                    fRockTerrain = "Yes";
+                } //End of if statement
+            } //End of onClick
+        }); //End of onClickListener
+
+        //Checks checkbox for values then converts values into strings
+        tLowBar.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(tLowBar.isChecked()) {
+                    fLowBar = "Yes";
+                } //End of if statement
+            } //End of onClick
+        }); //End of onClickListener
+
+        //Checks checkbox for values then converts values into strings
+        tAutoHigh.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(tAutoHigh.isChecked()) {
+                    fAutoHigh = "Yes";
+                } //End of if statement
+            } //End of onClick
+        }); //End of onClickListener
+
+        //Checks checkbox for values then converts values into strings
+        tAutoLow.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(tAutoLow.isChecked()) {
+                    fAutoLow = "Yes";
+                } //End of if statement
+            } //End of onClick
+        }); //End of onClickListener
+
+        //Checks checkbox for values then converts values into strings
+        tTeleHigh.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(tTeleHigh.isChecked()) {
+                    fTeleHigh = "Yes";
+                } //End of if statement
+            } //End of onClick
+        }); //End of onClickListener
+
+        //Checks checkbox for values then converts values into strings
+        tTeleLow.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(tTeleLow.isChecked()) {
+                    fTeleLow = "Yes";
+                } //End of if statement
+            } //End of onClick
+        }); //End of onClickListener
+
+        //Checks checkbox for values then converts values into strings
+        tHang.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(tHang.isChecked()) {
+                    fHang = "Yes";
+                } //End of if statement
+            } //End of onClick
+        }); //End of onClickListener
 
         //Adds data to ScoutFragment
         addDataButton = (Button) view.findViewById(R.id.buttonDataAdd);
@@ -87,22 +241,22 @@ public class AddScoutDataFragment extends Fragment {
 
     public void addScoutInfo() {
         //Converts all editText values into strings
-        String sNumber = tNumber.getText().toString();
-        String sPortcullis = tPortcullis.getText().toString();
-        String sChevalFrise = tChevalFrise.getText().toString();
-        String sMoat = tMoat.getText().toString();
-        String sRamparts = tRamparts.getText().toString();
-        String sDrawbridge = tDrawbridge.getText().toString();
-        String sSallyPort = tSallyPort.getText().toString();
-        String sRockWall = tRockWall.getText().toString();
-        String sRockTerrain = tRockTerrain.getText().toString();
-        String sLowBar = tLowBar.getText().toString();
-        String sAutoHigh = tAutoHigh.getText().toString();
-        String sAutoLow = tAutoLow.getText().toString();
-        String sTeleHigh = tTeleHigh.getText().toString();
-        String sTeleLow = tTeleLow.getText().toString();
+        String sNumber = tNumber.getText().toString();;
+        String sPortcullis = fPorticullis;
+        String sChevalFrise = fChevalFrise;
+        String sMoat = fMoat;
+        String sRamparts = fRamparts;
+        String sDrawbridge = fDrawbridge;
+        String sSallyPort = fSallyPort;
+        String sRockWall = fRockWall;
+        String sRockTerrain = fRockTerrain;
+        String sLowBar = fLowBar;
+        String sAutoHigh = fAutoHigh;
+        String sAutoLow = fAutoLow;
+        String sTeleHigh = fTeleHigh;
+        String sTeleLow = fTeleLow;
         String sTelePlay = tTelePlay.getText().toString();
-        String sHang = tHang.getText().toString();
+        String sHang = fHang;
 
         //Saves data
         sqLiteDatabase = myDB.getWritableDatabase();
